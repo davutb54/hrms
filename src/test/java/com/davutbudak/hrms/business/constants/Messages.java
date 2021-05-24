c263f8a871626e19d6852099c195a75b77d0e486
davut <54davutbudak@gmail.com>
2021-05-25 00:23:29 +0300

    Messages.java Added

---
+++ src/test/java/com/davutbudak/hrms/business/constants/Messages.java
@@ -0,0 +1,17 @@
+package com.davutbudak.hrms.business.constants;
+
+public class Messages {
+    // Job Position
+    public static String JOB_POSITION_DATA_LISTED = "İş pozisyonları listelendi";
+    public static String JOB_POSITION_ADDED = "İş pozizyonu eklendi";
+    public static String JOB_POSITION_ADD_ERROR = "İş pozisyonu eklenemedi";
+    public static String JOB_POSITION_UPDATED = "İş pozisyonu güncellendi";
+    public static String JOB_POSITION_UPDATE_ERROR = "İş pozisyonu güncellenemedi";
+    public static String JOB_POSITION_DELETED = "İş pozisyonu silindi";
+    public static String JOB_POSITION_DELETE_ERROR = "İş pozisyonu silinemedi";
+
+    //Candidate
+    public static String CANDIDATE_DATA_LISTED = "Adaylar listelendi";
+    public static String CANDIDATE_ADDED = "Aday eklendi";
+    public static String CANDIDATE_ADD_ERROR = "Aday eklenemedi";
+}

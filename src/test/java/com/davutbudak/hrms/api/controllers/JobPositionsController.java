66bcfc8bd9f17625c0cfb090166be7aa4dec81a5
davut <54davutbudak@gmail.com>
2021-05-25 00:26:22 +0300

    JobPositionsController Added

---
+++ src/test/java/com/davutbudak/hrms/api/controllers/JobPositionsController.java
@@ -0,0 +1,42 @@
+
+package com.davutbudak.hrms.api.controllers;
+
+import com.davutbudak.hrms.business.abstracts.JobPositionService;
+import com.davutbudak.hrms.core.utilities.results.DataResult;
+import com.davutbudak.hrms.core.utilities.results.Result;
+import com.davutbudak.hrms.entities.concretes.JobPosition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/jobpositions")
+public class JobPositionsController {
+    private JobPositionService jobPositionService;
+
+    @Autowired
+    public JobPositionsController(JobPositionService jobPositionService) {
+        this.jobPositionService = jobPositionService;
+    }
+
+    @GetMapping("/getall")
+    public DataResult<List<JobPosition>> getAll(){
+        return jobPositionService.getAll();
+    }
+
+    @PostMapping("/add")
+    public Result add(@RequestBody JobPosition jobPosition){
+        return this.jobPositionService.add(jobPosition);
+    }
+
+    @PostMapping("/update")
+    public Result update(@RequestBody JobPosition jobPosition){
+        return this.jobPositionService.update(jobPosition);
+    }
+
+    @PostMapping("/delete")
+    public Result delete(@RequestBody JobPosition jobPosition){
+        return this.jobPositionService.delete(jobPosition);
+    }
+}

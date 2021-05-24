7636e3a3feee68ff727e638de7aec4d564cfeffb
davut <54davutbudak@gmail.com>
2021-05-25 00:18:19 +0300

    Add Result

---
+++ src/test/java/com/davutbudak/hrms/core/utilities/results/DataResult.java
@@ -0,0 +1,18 @@
+package com.davutbudak.hrms.core.utilities.results;
+
+import lombok.Getter;
+
+@Getter
+public class DataResult<T> extends Result {
+    private T data;
+
+    public DataResult(T data, boolean success, String message) {
+        super(success, message);
+        this.data = data;
+    }
+
+    public DataResult(T data, boolean success) {
+        super(success);
+        this.data = data;
+    }
+}
---
+++ src/test/java/com/davutbudak/hrms/core/utilities/results/ErrorDataResult.java
@@ -0,0 +1,12 @@
+package com.davutbudak.hrms.core.utilities.results;
+
+public class ErrorDataResult<T> extends DataResult<T> {
+
+    public ErrorDataResult(T data) {
+        super(data, false);
+    }
+
+    public ErrorDataResult(T data, String message) {
+        super(data, false, message);
+    }
+}

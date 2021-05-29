f440ad51375af2888c109af5fd55816ac85af876
davut <54davutbudak@gmail.com>
2021-05-29 18:03:39 +0300

    New Entities Added

---
+++ src/main/java/com/davutbudak/hrms/entities/concretes/JobAdvertisement.java
@@ -0,0 +1,55 @@
+package com.davutbudak.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.swing.*;
+import java.sql.Date;
+
+@Data
+@Entity
+@Table(name = "job_advertisement")
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobAdvertisement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "employer_id")
+    private Employer employer;
+
+    @ManyToOne
+    @JoinColumn(name = "job_position_id")
+    private JobPosition jobPosition;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    @Column(name = "job_description")
+    private String jobDescription;
+
+    @Column(name = "min_salary")
+    private int minSalary;
+
+    @Column(name = "max_salary")
+    private int maxSalary;
+
+    @Column(name = "number_of_open_positions")
+    private int numberOfOpenPositions;
+
+    @Column(name = "release_date")
+    private Date releaseDate;
+
+    @Column(name = "deadline")
+    private Date deadline;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+}
